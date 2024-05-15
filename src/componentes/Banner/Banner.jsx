c953e47bc0389d405b6c68a624fe1bd0f6ff469e
Felipe Everardo <fesyger@gmail.com>
2024-05-15 19:52:04 -0300

    iniciando projeto

---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+
+import banner from "../../../public/imagens/banner.png"
+
+function Banner() {
+    console.log(banner);
+    return (
+        <img src={banner} alt="O banner principal da página do "/>
+    )
+}
+
+export default Banner

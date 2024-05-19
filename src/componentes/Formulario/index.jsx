cdf36098c36f216fea519f79e09726c33ae05088
Felipe Everardo <fesyger@gmail.com>
2024-05-19 16:31:16 -0300

    botao incluso

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,14 +1,33 @@
 import './Formulario.css'
-import CampoTexto from '../CampoTexto/CampoTexto'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao/Botao'
+
 
 const Formulario = (props) => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data-Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+
     return (
         <section className='formulario'>
             <form>
-            <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
                 <CampoTexto label='Nome' placeholder='Digite seu nome' />
                 <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
                 <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
+                <ListaSuspensa label='Time' itens={times} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
